4e2c82b7cf2fcde5e26b8c096c4453826ad33c60
Vinicius Sousa Palmeira <142844574+Vini09090@users.noreply.github.com>
2026-09-22 20:19:15 -0300

    Create Iniciar.py

---
+++ Alexandria/Dados/Iniciar.py
@@ -0,0 +1,30 @@
+from .banco import db, Usuário
+
+#Criando as tabelas do banco de dados.
+
+db.connect()
+db.create_tables([Usuário])
+
+class perfil:
+    def __init__(self, nome, senha):
+        self.nome = nome
+        self.senha = senha
+
+    def criar_conta(self) -> bool:
+        try:
+            Usuário.create(
+                nome=self.nome,
+                password=self.senha
+            )
+            return True
+        except:
+            return False
+
+    def verificar(self) -> bool:
+        usuario = Usuário.get_or_none(
+            (Usuário.nome == self.nome) &
+            (Usuário.password == self.senha)
+        )
+
+        return usuario is not None
+    
